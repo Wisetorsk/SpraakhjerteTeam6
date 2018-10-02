fe564818aadc9c3d2d891404dc1df75419a5306e
Marius S Kristiansen <mariussk@getacademy.no>
2018-10-02 19:18:52 +0200

    Javascript updates

---
+++ marius.js
@@ -0,0 +1,39 @@
+// JavaScript source code
+var antallKort = 10;
+var kortstokk = [];
+var kategorier = ['klær', 'mat', 'kjøretøy', 'skole', 'verktøy'];
+var antKategorier = kategorier.length;
+
+class kort {
+    constructor(kategori, navn) {
+        this.width = 70;
+        this.height = 70;
+        this.kategori = kategori;
+        this.navn = navn;
+    }
+
+    display() { 
+        //console.log(this.kategori);
+        //return this.kategori;
+    }
+
+    place() {
+        //
+    }
+}
+
+function trekk() {
+    document.getElementById('demokort').innerHTML = kortstokk[randomInt(antKategorier)].kategori;
+}
+
+function main() {
+    for (var i = 0; i < antallKort; i++) {
+        kortstokk[i] = new kort(kategorier[randomInt(antKategorier)], 'TEST');
+    }
+}
+
+function randomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+main();
