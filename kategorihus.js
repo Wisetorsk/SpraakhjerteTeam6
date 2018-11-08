371ec90d4c12e8b5847a7132d5963877c2ee87b2
Marius S Kristiansen <mariussk@getacademy.no>
2018-11-08 12:38:13 +0100

    Update kategorihus.js

--- kategorihus.js
+++ kategorihus.js
@@ -319,10 +319,10 @@ function speak(word) {
     let audio = new Audio();
     switch (language) {
         case "norwegian":
-            audio.src = 'https://translate.google.com/translate_tts?ie=UTF-8&tl=nb-NO&client=tw-ob&q=' + word;
+            audio.src = 'https://translate.google.com/translate_tts?ie=UTF-8&tl=nb-NO&client=tw-ob&q="' + word + '"';
             break;
         case "english":
-            audio.src = 'https://translate.google.com/translate_tts?ie=UTF-8&tl=en-US&client=tw-ob&q=' + word;
+            audio.src = 'https://translate.google.com/translate_tts?ie=UTF-8&tl=en-US&client=tw-ob&q="' + word + '"';
             break;
     }
 
